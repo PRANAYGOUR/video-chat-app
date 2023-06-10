b3990d6befb9f2adfa47a89d63cb4ba78b73c11e
PRANAYGOUR <76206607+PRANAYGOUR@users.noreply.github.com>
2023-06-10 13:25:38 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -21,36 +21,27 @@ navigator.mediaDevices
     .then((stream) => {
         myStream = stream;
         addVideoStream(myVideo, stream);
-        socket.on("user-connected" , (userId)=>{
-            coonnectNewUser(userId,stream)
-        })
-    peer.on("call" , (call)=>{
-        call.answer(stream)
-        const video = document.createElement("video")
-        call.on("stream" , (userVideoStream)=>{
-            addVideoStream(video,userVideoStream)
-        })
 
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
     })
-    })
 
-
-function coonnectNewUser(userId,stream) {
-    const call = peer.call(userId,stream)
-    const video = document.createElement("video")
-    call.on("stream" , (userVideoStream) =>{
-        addVideoStream(video,userVideoStream)
-    })
-}
-
-//we have two secnarios when we join a room. there are already people waiting in room for vieo chat another scenario is someone new coming the room 
-//when we connect to new user function we are actually new to room and trying to make call to the people who are already in the room
-//so that they can see our video and audio and you can see the others people video and audio using stream event inside call.on
-
-
-
-
-
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
 
 function addVideoStream(video, stream) {
     video.srcObject = stream;
@@ -85,59 +76,57 @@ $(function () {
             $("#chat_message").val("");
         }
     })
-    $("#mute_button").keydown(function(){
-        const enabled = myStream.getAudioTracks()[0].enabled
-        if(enabled){
-            myStream.getAudioTracks()[0].enabled=false
-            html=`<i class="fas fa-microphone-slash"></i>`
-            $("#mute_button").toggleClass("background_red")
+
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
         }
-        else{
-            myStream.getAudioTracks()[0].enabled=true
-            html=`<i class="fas fa-microphone"></i>`
-            $("#mute_button").html(html)
-
-        }
-
     })
-    $("#stop_video").keydown(function(){
-        const enabled = myStream.getVideoTracks()[0].enabled
-        if(enabled){
-            myStream.getVideoTracks()[0].enabled=false
-            html=`<i class="fas fa-video-slash"></i>`
-            $("#stop_video").toggleClass("background_red")
+
+    $("#stop_video").click(function () {
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
         }
-        else{
-            myStream.getVideoTracks()[0].enabled=true
-            html=`<i class="fas fa-video"></i>`
-            $("#stop_video").html(html)
-
-        }
-        
     })
-    $("#invite_button").click(function(){
-        const to = prompt("enter the email address")
+
+    $("#invite_button").click(function () {
+        const to = prompt("Enter the email address")
         let data = {
-            url:window.location.href,
-            to:to
+            url: window.location.href,
+            to: to
         }
         $.ajax({
-            url:"/send-mail",
-            type:"post",
-            data:JSON.stringify(data),
-            dataType:"json",
-            contentType:"application/json",
-            success:function(result){
-                alert("Invite sent")
+            url: "/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (result) {
+                alert("Invite sent!")
             },
-            error:function(result){
+            error: function (result) {
                 console.log(result.responseJSON)
             }
         })
     })
-
 
 })
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json())
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -15,22 +16,21 @@ const io = require("socket.io")(server, {
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
     debug: true,
-
 });
 
-var nodemailer = require('nodemailer')
-const transporter = nodemailer.createTransport({
-    port:587,
-    host:"smtp.gmail.com",
-    auth:{
-        user:'pranaygour3042007@gmail.com',
-        pass:'eigwgmqgekwrdear'
-    },
-    secure:true,
-})
-
-
 app.use("/peerjs", peerServer);
+
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: '',
+        pass: '',
+    },
+    secure: true,
+});
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -40,31 +40,27 @@ app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
 
-app.post("/send-mail" , (req,res)=>{
+app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
-    const maildata = {
-        from:"pranaygour3042007@gmail.com",
-        to:to,
-        subject:"Join the Video chat with me",
-        html:`<p>Hey There</p>
-        <p>come and join with me for a video chat here - ${url}</p>`
+    const mailData = {
+        from: "apoorv.goyal@whitehatjr.com",
+        to: to,
+        subject: "Join the video chat with me!",
+        html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
     };
-    transporter.sendMail(maildata,(error,info)=>{
-        if(error){
-            return console.log(error)
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
         }
-        else{
-            res.status(200).send({message:"Invitation sent" , message_id:info.messageId})
-        }
-    })
-
+        res.status(200).send({ message: "Invitation sent!", message_id: info.messageId });
+    });
 })
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
-        io.to(roomId).emit("user-conncted" , userId)
+        io.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
